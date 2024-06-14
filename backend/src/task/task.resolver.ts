3ed587615460cd4ae78cc5ef1373e34c21b7c3bc
r-numahata-sysnavi <106241953+r-numahata-sysnavi@users.noreply.github.com>
2024-06-14 04:05:28 +0000

    create graphql project

---
+++ backend/src/task/task.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateTaskInput } from './dto/createTask.input';
+import { Task as TaskModel } from './models/task.model';
+import { TaskService } from './task.service';
+import { Task } from '@prisma/client';
+
+@Resolver()
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Query(() => [TaskModel], { nullable: 'items' })
+  async getTasks(): Promise<Task[]> {
+    return await this.taskService.getTasks();
+  }
+
+  @Mutation(() => TaskModel)
+  async createTask(
+    @Args('createTaskInput') createTaskInput: CreateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.createTask(createTaskInput);
+  }
+}
---
+++ backend/src/task/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskInput } from './dto/createTask.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Status, Task } from '@prisma/client';
+
+@Injectable()
+export class TaskService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getTasks(): Promise<Task[]> {
+    return await this.prismaService.task.findMany();
+  }
+  async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
+    const { name, dueDate, description } = createTaskInput;
+    return await this.prismaService.task.create({
+      data: {
+        name,
+        dueDate,
+        description,
+        status: Status.NOT_STARTED,
+      },
+    });
+  }
+}
