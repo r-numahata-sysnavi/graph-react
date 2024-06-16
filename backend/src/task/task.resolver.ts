2ebdd38a4cc295ab45d02203453094bd6bb68f92
r-numahata-sysnavi <106241953+r-numahata-sysnavi@users.noreply.github.com>
2024-06-16 14:34:42 +0000

    add jwt auth

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,21 +1,27 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateTaskInput } from './dto/createTask.input';
 import { Task as TaskModel } from './models/task.model';
 import { TaskService } from './task.service';
 import { Task } from '@prisma/client';
 import { UpdateTaskInput } from './dto/updateTask.input';
 import { DeleteTaskInput } from './dto/deleteTask.input';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
 export class TaskResolver {
   constructor(private readonly taskService: TaskService) {}
 
   @Query(() => [TaskModel], { nullable: 'items' })
-  async getTasks(): Promise<Task[]> {
-    return await this.taskService.getTasks();
+  @UseGuards(JwtAuthGuard)
+  async getTasks(
+    @Args('userId', { type: () => Int }) userId: number,
+  ): Promise<Task[]> {
+    return await this.taskService.getTasks(userId);
   }
 
   @Mutation(() => TaskModel)
+  @UseGuards(JwtAuthGuard)
   async createTask(
     @Args('createTaskInput') createTaskInput: CreateTaskInput,
   ): Promise<Task> {
@@ -23,6 +29,7 @@ export class TaskResolver {
   }
 
   @Mutation(() => TaskModel)
+  @UseGuards(JwtAuthGuard)
   async updateTask(
     @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
   ): Promise<Task> {
@@ -30,6 +37,7 @@ export class TaskResolver {
   }
 
   @Mutation(() => TaskModel)
+  @UseGuards(JwtAuthGuard)
   async deleteTask(
     @Args('deleteTaskInput') deleteTaskInput: DeleteTaskInput,
   ): Promise<Task> {
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -9,17 +9,22 @@ import { DeleteTaskInput } from './dto/deleteTask.input';
 export class TaskService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getTasks(): Promise<Task[]> {
-    return await this.prismaService.task.findMany();
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prismaService.task.findMany({
+      where: {
+        userId,
+      },
+    });
   }
   async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
-    const { name, dueDate, description } = createTaskInput;
+    const { name, dueDate, description, userId } = createTaskInput;
     return await this.prismaService.task.create({
       data: {
         name,
         dueDate,
         description,
         status: Status.NOT_STARTED,
+        userId,
       },
     });
   }
